fc4a3e6c4de256357ee0befb7a4986833bbd2dd0
yousuke-nukui <60282938+yousuke-nukui@users.noreply.github.com>
2020-02-25 13:58:40 +0900

    Add files via upload

---
+++ home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  def top
+  end
+  def howto
+  end
+  def login
+  end
+  def new
+  end
+  def recommend
+
+  end
+  def signup
+
+  end
+  def write
+
+  end
+
+end
