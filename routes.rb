0674a33ed70347d7e175e0462f5557a08fcf6e90
yosuke_nukui <yosuke10110@gmail.com>
2020-02-27 20:48:56 +0900

    posts/showを追加

--- routes.rb
+++ routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'posts/index' => 'posts#index'
+  get 'posts/:id' => 'posts#show'
   get '/' => 'home#top'
   get 'howto' => 'home#howto'
   get 'login' => 'home#login'
