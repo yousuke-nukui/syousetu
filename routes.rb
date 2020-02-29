9f46abcc37d72aff9df142ccf86d06fa9c897a70
tantantom <soreikenogi12@gmail.com>
2020-02-29 14:57:52 +0900

    投稿機能

--- routes.rb
+++ routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index' => 'posts#index'
-  get 'posts/:id' => 'posts#show'
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+
   get '/' => 'home#top'
   get 'howto' => 'home#howto'
   get 'login' => 'home#login'
