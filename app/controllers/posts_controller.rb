1764f3ea4a882a6be2ce7b7b958ba5ecc4348f85
yosuke_nukui <yosuke10110@gmail.com>
2020-03-06 16:26:38 +0900

    ログアウト機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find_by(id: params[:id])
-    @post.content=params[:content]
+    @post.content = params[:content]
     @post.save
     redirect_to("/posts/index")
   end
