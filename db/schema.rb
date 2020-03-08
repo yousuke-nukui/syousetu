233b91b64d610478f9073b0bebd96f4fd6b0c9f0
yosuke_nukui <yosuke10110@gmail.com>
2020-03-08 14:00:48 +0900

    パスワード暗号化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_080930) do
+ActiveRecord::Schema.define(version: 2020_03_08_022943) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_080930) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
